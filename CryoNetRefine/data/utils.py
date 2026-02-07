9b6a7623e268c179a1a66e1a1593efb22ed4713b
Kui Xu <xukui.cs@gmail.com>
2026-02-07 14:23:25 +0800

    add update_status

---
+++ CryoNetRefine/data/utils.py
@@ -0,0 +1,22 @@
+import os,json
+from pathlib import Path
+
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+def update_status(pdb_dir, jdict):
+    # 
+    # jdict = {'msg': 'Out of memory', 'error_code':0, "stg": 5, "progress": 10}
+    sfile = f'{pdb_dir}/status'
+    if Path(sfile).exists():
+        jdata = json.load(open(sfile))
+        jdata.update(jdict)
+        json.dump(jdata, open(sfile, 'w'), indent=4, cls=NpEncoder)
